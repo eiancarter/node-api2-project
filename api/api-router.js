90608de2e979f3d5cf77e17f34ece80720aec05a
Eian Carter <eian@bricleir.com>
2020-02-11 16:33:47 -0800

    mvp reached and project formatted professionally

--- api/api-router.js
+++ api/api-router.js
@@ -1,3 +1,11 @@
 const express = require("express");
 
 const postsRouter = require("../posts/posts-router.js");
+
+const router = express.Router();
+
+
+
+router.use("/", postsRouter);
+
+module.exports = router;
